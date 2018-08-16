0a0f69a6047fd868c721b561aa92f19fff675a41
haneyleg <42439878+haneyleg@users.noreply.github.com>
2018-08-16 22:46:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,39 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.'
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[Codes] ${client.users.size}`)
-    client.user.setStatus("idle")
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
-
-
-
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**
 **WELCOME TO C A S A N O V A  :hearts:️:dove:**
-
-        [https://discord.gg/Kx6w6pa]                            
+        https://discord.gg/KhP6eH5                            
 الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
-**`)
+`)
 }).catch(console.error)
 
 })
@@ -27,25 +41,20 @@ client.on('ready', () => {
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**
 **WELCOME TO C A S A N O V A  :hearts:️:dove:**
-
-        [https://discord.gg/Kx6w6pa]                            
+        https://discord.gg/KhP6eH5                           
 الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
-**`) 
+`) 
 }).catch(console.error)
 })
-
 
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**
 **WELCOME TO C A S A N O V A  :hearts:️:dove:**
-
-        [https://discord.gg/Kx6w6pa]                            
+        https://discord.gg/KhP6eH5                            
 الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
-**`)
+`)
 }).catch(console.error)
 
 })
@@ -53,12 +62,10 @@ client.on("guildMemberAdd", member => {
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**
 **WELCOME TO C A S A N O V A  :hearts:️:dove:**
-
-        [https://discord.gg/Kx6w6pa]                            
+        https://discord.gg/KhP6eH5                           
 الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
-**`) 
+`) 
 }).catch(console.error)
 })
 
