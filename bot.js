8840a7199c3643d791b995f2fb2d1738837bfe4e
haneyleg <42439878+haneyleg@users.noreply.github.com>
2018-08-16 22:16:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,9 +16,9 @@ client.on('ready', () => {
   return channel.send(` 
 **
 **WELCOME TO C A S A N O V A  :hearts:️:dove:**
-C A S A N O V A 
+
         [https://discord.gg/Kx6w6pa]                            
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
 **`)
 }).catch(console.error)
 
@@ -29,9 +29,35 @@ client.on("guildMemberAdd", member => {
   return channel.send(` 
 **
 **WELCOME TO C A S A N O V A  :hearts:️:dove:**
-C A S A N O V A
+
         [https://discord.gg/Kx6w6pa]                            
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+**WELCOME TO C A S A N O V A  :hearts:️:dove:**
+
+        [https://discord.gg/Kx6w6pa]                            
+الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+**WELCOME TO C A S A N O V A  :hearts:️:dove:**
+
+        [https://discord.gg/Kx6w6pa]                            
+الدعووة خاصة لك ي قلبي وعيني ... [ ${member}  ]
 **`) 
 }).catch(console.error)
 })
